4b57b348146884647b5ad18db82241c69f533e70
sodatea <haoqunjiang@gmail.com>
2020-04-09 02:18:35 +0000

    Deploying to gh-pages from master f9bfbe5ac9eb65d4296e63b70e82a92eccb7f361

---
+++ precache-manifest.baad41946dd5f61c275d32dda29e6024.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97df17c7c6d016fb93e",
+    "url": "/vue-jsx-playground/css/app.d8b75244.css"
+  },
+  {
+    "revision": "6245b28d5b6f52573be1",
+    "url": "/vue-jsx-playground/css/chunk-vendors.2499afe2.css"
+  },
+  {
+    "revision": "6456b5b0154805468801cc2369afed2c",
+    "url": "/vue-jsx-playground/index.html"
+  },
+  {
+    "revision": "d97df17c7c6d016fb93e",
+    "url": "/vue-jsx-playground/js/app.52f0e66f.js"
+  },
+  {
+    "revision": "7f7816915dafc8b36f93",
+    "url": "/vue-jsx-playground/js/chunk-2d0c51a5.5bdded95.js"
+  },
+  {
+    "revision": "9254849b7ff9beb8191e",
+    "url": "/vue-jsx-playground/js/chunk-5b5f63d0.bdfd4fa5.js"
+  },
+  {
+    "revision": "6245b28d5b6f52573be1",
+    "url": "/vue-jsx-playground/js/chunk-vendors.81071c34.js"
+  },
+  {
+    "revision": "cb94a3c4d5930d7fa094b17766a1d209",
+    "url": "/vue-jsx-playground/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-jsx-playground/robots.txt"
+  }
+]);
